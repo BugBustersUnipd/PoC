47f21dd53c08176a01d07f00bb1a20d11c0add83
Alberto <alberto.autiero2003@gmail.com>
2026-01-16 09:56:43 +0100

    Rimosse pagine Modifica Generazione e Aicopilotconfidenza

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,8 +3,6 @@ import { AiAssistant } from './pages/ai-assistant/ai-assistant';
 import { AiCopilot } from './pages/ai-copilot/ai-copilot';
 import { StoricoPrompt } from './pages/storico-prompt/storico-prompt';
 import { RisultatoGenerazione } from './pages/risultato-generazione/risultato-generazione';
-import { ModificaGenerazione } from './pages/modifica-generazione/modifica-generazione';
-import { Aicopilotconfidenza } from './pages/aicopilotconfidenza/aicopilotconfidenza';
 import { Aicopilotanteprimadocumento } from './pages/aicopilotanteprimadocumento/aicopilotanteprimadocumento';
 
 export const routes: Routes = [
@@ -12,8 +10,6 @@ export const routes: Routes = [
   { path: 'ai-copilot', component: AiCopilot },
   { path: 'storico-prompt', component: StoricoPrompt },
   { path: 'risultato-generazione', component: RisultatoGenerazione },
-  { path: 'modifica-generazione', component: ModificaGenerazione },
-  { path: 'aicopilotconfidenza', component: Aicopilotconfidenza },
   { path: 'aicopilotanteprimadocumento', component: Aicopilotanteprimadocumento },
   { path: 'aicopilotanteprima/:id', component: Aicopilotanteprimadocumento },
 ];
