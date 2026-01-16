4d4d779ce890abe022bf42aa71bddd2a6c462967
Alberto <alberto.autiero2003@gmail.com>
2026-01-16 09:54:12 +0100

    Rimossa pagina Analytics

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { Analytics } from './pages/analytics/analytics';
 import { AiAssistant } from './pages/ai-assistant/ai-assistant';
 import { AiCopilot } from './pages/ai-copilot/ai-copilot';
 import { StoricoPrompt } from './pages/storico-prompt/storico-prompt';
@@ -9,7 +8,6 @@ import { Aicopilotconfidenza } from './pages/aicopilotconfidenza/aicopilotconfid
 import { Aicopilotanteprimadocumento } from './pages/aicopilotanteprimadocumento/aicopilotanteprimadocumento';
 
 export const routes: Routes = [
-  { path: 'analytics', component: Analytics },
   { path: 'ai-assistant', component: AiAssistant },
   { path: 'ai-copilot', component: AiCopilot },
   { path: 'storico-prompt', component: StoricoPrompt },
